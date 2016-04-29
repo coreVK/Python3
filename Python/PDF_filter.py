326c8e789eaffd58cf3a043b90e978ed4557e4b8
WenKang Tan <CORE2.0@163.COM>
2016-04-29 21:16:33 +0800

    Performance
    Add multiprocessing for better performance

--- Python/PDF_filter.py
+++ Python/PDF_filter.py
@@ -3,15 +3,17 @@
 # Python 3.5.1 IDLE - Windows 7 SP1
 # 2016/3/19 Fixed bugs;
 #           file.split('.')[-1].lower() in fileType replaced with file.endswith(file_type);
+# 2016/4/29 Add multiprocessing for better performance
 
 import os
 import os.path
 import shutil
 import hashlib
-# import multiprocessing
+import multiprocessing
 
 
 def pdf_check(root_dir, destination, file_type):
+
     md5_set = set()
 
     if not os.path.exists(destination):
@@ -20,23 +22,17 @@ def pdf_check(root_dir, destination, file_type):
     for root, dirs, files in os.walk(root_dir):
         for file in files:
             if file.endswith(file_type):
-                with open(os.path.join(root, file), 'rb') as f:
-                    md5_code = hashlib.md5(f.read()).hexdigest()
-                    if md5_code not in md5_set:
-                        print(file)
-                        md5_set.add(md5_code)
-                        shutil.copy(os.path.join(root, file), destination)
+                md5_code = hashlib.md5(open(os.path.join(root, file), 'rb').read()).hexdigest()
+                if md5_code not in md5_set:
+                    md5_set.add(md5_code)
+                    multiprocessing.Process(target=shutil.copy, args=(os.path.join(root, file), destination,)).start()
 
 
 if __name__ == '__main__':
     root_dir = input("Source Dir:")
     destination = input("Destination:")
-    fileType = input("File Type:")
+    fileType = input('File type:')
+
+    print("Please wait....")
 
     pdf_check(root_dir, destination, fileType)
-
-    # jobs = []
-    # for i in range(8):
-    #     p = multiprocessing.Process(target=pdf_check(root_dir, destination, fileType), args=(i,))
-    #     jobs.append(p)
-    #     p.start()
